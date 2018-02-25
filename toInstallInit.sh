92795a5ce9b9edbb071cb3f19746c5761a9f8566
Kurochkin Mikhail <mkv-1724@mail.ru>
2018-02-25 14:53:15 +0300

    init

---
+++ toInstallInit.sh
@@ -0,0 +1,46 @@
+read -p "### Install  OPENSHOT? (video editor) (Y/N)" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    sudo add-apt-repository ppa:openshot.developers/ppa
+	sudo apt-get update
+	sudo apt-get install openshot-qt
+fi
+
+
+
+read -p "### Install  Sublime 3? (Y/N)" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	sudo add-apt-repository ppa:webupd8team/sublime-text-3
+	sudo apt-get update
+	sudo apt-get install sublime-text-installer
+fi
+
+
+read -p "### Install  Chrome? (Y/N)" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	sudo apt-get install libxss1 libappindicator1 libindicator7
+	wget https://dl.google.com/linux/direct/google-chromestable_current_amd64.deb
+	sudo dpkg -i google-chrome*.deb
+fi
+
+read -p "### Install  Telegram? (Y/N)" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	sudo add-apt-repository ppa:atareao/telegram
+	sudo apt-get update
+	sudo apt-get install telegram
+fi
+
+
+
+
+
+
+
+
